b26a058e3345101cba70871bc3ac70bb3d82cb25
Ashley Thibodeau <AThibodeau1@Student.Fullsail.edu>
2021-04-29 16:32:31 -0400

    added js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 // JavaScript Document
+let userResponse = false;
 
-alert("Hello! ");
+if (confirm('Hello! Are you a devloper interested in working with me?')) {
+  //
+	alert('OK Great! Please open your browsers console log to continue...');
+	console.log('Challenge Accepted! To further work with me I ask you play a game');
+	userResponse = true;
+} 
+else {
+  // Do nothing!
+  console.log('User is not interested.');
+	userResponse = false
+}
+
+while(userResponse == true)
+{
+	
+
+}
